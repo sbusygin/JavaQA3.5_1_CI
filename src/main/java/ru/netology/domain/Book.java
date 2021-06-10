a5828a6207c851bacfccbc8985a567c90d892b11
Sergey Busygin <tranebus3@yandex.ru>
2021-06-10 14:13:33 +0300

    myWork

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -3,67 +3,44 @@ package ru.netology.domain;
 import java.util.Objects;
 
 public class Book extends Product {
-  private String author;
-  private int pages;
-  private int publishedYear;
+    private String author;
 
-  public Book() {
-    super();
-  }
+    public Book() {
+        super();
+    }
 
-  public Book(int id, String name, int price, String author, int pages, int publishedYear) {
-    super(id, name, price);
-    this.author = author;
-    this.pages = pages;
-    this.publishedYear = publishedYear;
-  }
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
 
-  public String getAuthor() {
-    return author;
-  }
+    public String getAuthor() {
+        return author;
+    }
 
-  public void setAuthor(String author) {
-    this.author = author;
-  }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
-  public int getPages() {
-    return pages;
-  }
 
-  public void setPages(int pages) {
-    this.pages = pages;
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author);
+    }
 
-  public int getPublishedYear() {
-    return publishedYear;
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), author);
+    }
 
-  public void setPublishedYear(int publishedYear) {
-    this.publishedYear = publishedYear;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    if (!super.equals(o)) return false;
-    Book book = (Book) o;
-    return pages == book.pages &&
-        publishedYear == book.publishedYear &&
-        Objects.equals(author, book.author);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(super.hashCode(), author, pages, publishedYear);
-  }
-
-  @Override
-  public String toString() {
-    return "Book{" +
-        "author='" + author + '\'' +
-        ", pages=" + pages +
-        ", publishedYear=" + publishedYear +
-        '}';
-  }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/ru/netology/domain/ProductManager.java
@@ -0,0 +1,53 @@
+package ru.netology.domain;
+
+import ru.netology.repository.ProductRepository;
+
+public class ProductManager {
+    ProductRepository productRepository;
+
+    public ProductManager(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void add(Product product) {
+        productRepository.save(product);
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product: productRepository.findAll()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product instanceof Book) {
+            Book book = (Book) product;
+            if (book.getAuthor().contains(search)) {
+                return true;
+            }
+            if (book.getName().contains(search)) {
+                return true;
+            }
+            return false;
+        }
+        if (product instanceof Smartphone){
+            Smartphone smartphone = (Smartphone) product;
+            if (smartphone.getManufacturer().contains(search)){
+                return true;
+            }
+            if (smartphone.getName().contains(search)){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+}
